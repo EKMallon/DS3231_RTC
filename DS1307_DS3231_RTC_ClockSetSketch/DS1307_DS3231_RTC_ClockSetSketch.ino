700bf123d94cea0915fa19839011a8ca488bbb62
EKMallon <EKMallon@users.noreply.github.com>
2014-06-24 17:21:16 -0500

    Format

--- DS1307_DS3231_RTC_ClockSetSketch/DS1307_DS3231_RTC_ClockSetSketch.ino
+++ DS1307_DS3231_RTC_ClockSetSketch/DS1307_DS3231_RTC_ClockSetSketch.ino
@@ -1,15 +1,15 @@
 /* ReatTimeClock_reset - Last updated Apr2014
-
-  Use this sketch to reset the DS1307 & DS3231 Real Time Clocks. It will grab
-  the computer's current time during compilation and send that value
-  to the real time clock. 
-  
-  Afterwards, immediately upload a DIFFERENT sketch to the Arduino
-  so that it doesn't try to constantly reset the clock when it 
-  powers up the next time. If it does reset, the Arduino will reset 
-  the clock with the old compile time stamp, which will be out of
-  date.
-*/
+ 
+ Use this sketch to reset the DS1307 & DS3231 Real Time Clocks. It will grab
+ the computer's current time during compilation and send that value
+ to the real time clock. 
+ 
+ Afterwards, immediately upload a DIFFERENT sketch to the Arduino
+ so that it doesn't try to constantly reset the clock when it 
+ powers up the next time. If it does reset, the Arduino will reset 
+ the clock with the old compile time stamp, which will be out of
+ date.
+ */
 
 // from https://github.com/MrAlvin/RTClib/blob/master/examples/settime/settime.pde
 
@@ -60,6 +60,7 @@ void loop(void)
   Serial.print(':');
   Serial.println(now.second(), DEC);
   delay(2000);
-  
+
 
 }
+
