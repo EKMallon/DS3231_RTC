58eee3f43c6be106483ec09625e503c9d42afab6
EKMallon <EKMallon@users.noreply.github.com>
2015-03-15 15:20:37 -1100

    Remove Serial monitor code

--- DS1307_DS3231_RTC_ClockSetSketch/DS1307_DS3231_RTC_ClockSetSketch.ino
+++ DS1307_DS3231_RTC_ClockSetSketch/DS1307_DS3231_RTC_ClockSetSketch.ino
@@ -4,6 +4,8 @@
  the computer's current time during compilation and send that value
  to the real time clock. 
  
+ HAVE THE SERIAL MONITOR OPEN WHEN YOU UPLOAD. You introduce a significant delay if you open it late.
+ 
  Afterwards, immediately upload a DIFFERENT sketch to the Arduino
  so that it doesn't try to constantly reset the clock when it 
  powers up the next time. If it does reset, the Arduino will reset 
@@ -16,6 +18,7 @@
 #include <Wire.h>
 #include <SPI.h>    // Not used here but required for this RTClib to compile properly
 #include <RTClib.h> // this is the library from https://github.com/MrAlvin/RTClib
+#define RTCPOWER_PIN 7
 
 RTC_DS1307 RTC;
 
@@ -23,14 +26,12 @@ boolean resetFlag = false; // flag for clock reset
 
 void setup(void)
 {
-  delay(2000);
+  pinMode(RTCPOWER_PIN, HIGH);   //some of my units use this pin for powering the RTC, need to set high for I2c coms.
   Wire.begin();
   RTC.begin();  
   RTC.adjust(DateTime(__DATE__, __TIME__));
-
-
   DateTime now = RTC.now();
-  Serial.begin(9600);
+  /*Serial.begin(9600);
   Serial.print(now.year(), DEC);
   Serial.print('/');
   Serial.print(now.month(), DEC);
@@ -41,8 +42,9 @@ void setup(void)
   Serial.print(':');
   Serial.print(now.minute(), DEC);
   Serial.print(':');
-  Serial.println(now.second(), DEC);  
-  delay(2000);
+  Serial.println(now.second(), DEC); 
+  */
+
 }
 
 void loop(void)
@@ -59,8 +61,19 @@ void loop(void)
   Serial.print(now.minute(), DEC);
   Serial.print(':');
   Serial.println(now.second(), DEC);
-  delay(2000);
-
+  //the leds just tell me the time has been updated without opening the serial window, which restarts the application
+  digitalWrite(4,HIGH);
+  delay(500);
+  digitalWrite(4,LOW);
+  delay(500);
+  digitalWrite(5,HIGH);
+  delay(500);
+  digitalWrite(5,LOW);
+  delay(500);
+  digitalWrite(6,HIGH);
+  delay(500);
+  digitalWrite(6,LOW);
+  delay(500);
 
 }
 
