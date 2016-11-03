b223cbba5bc0977246bb64195a22584ba8995c30
EKMallon <EKMallon@users.noreply.github.com>
2016-11-03 10:22:08 -0500

    Updates

--- DS1307_DS3231_RTC_ClockREADonlySketch/DS1307_DS3231_RTC_ClockREADonlySketch.ino
+++ DS1307_DS3231_RTC_ClockREADonlySketch/DS1307_DS3231_RTC_ClockREADonlySketch.ino
@@ -1,18 +1,9 @@
-/* ReatTimeClock_reset - Last updated Apr2014
- 
- Use this sketch to reset the DS1307 & DS3231 Real Time Clocks. It will grab
- the computer's current time during compilation and send that value
- to the real time clock. 
- 
- HAVE THE SERIAL MONITOR OPEN WHEN YOU UPLOAD. You introduce a significant delay if you open it late.
- 
- Afterwards, immediately upload a DIFFERENT sketch to the Arduino
- so that it doesn't try to constantly reset the clock when it 
- powers up the next time. If it does reset, the Arduino will reset 
- the clock with the old compile time stamp, which will be out of
- date.
+/* ReatTimeClock_reset - Last updated Apr2014 
+ Use this sketch to READ the time from DS1307 & DS3231 Real Time Clocks. 
  */
-//
+ 
+// to reduce your time lag on upload:
+// Arduino Trick: Double Upload Speed  https://www.sparkfun.com/news/1552  
 
 // from https://github.com/MrAlvin/RTClib/blob/master/examples/settime/settime.pde
 
@@ -20,17 +11,17 @@
 #include <SPI.h>    // Not used here but required for this RTClib to compile properly
 #include <RTClib.h> // this is the library from https://github.com/MrAlvin/RTClib
 
-#define RTCPOWER_PIN 7  //rockets
+#define RTCPOWER_PIN 7  //This pin poweres the DS3231 IC on rocket based builds
 #define RED_PIN 4    //pin settings for Rocket loggers
 #define GREEN_PIN 5 
 #define BLUE_PIN 6 
 
 //#define RTCPOWER_PIN 14  //motinos
-//#define RED_PIN 20    //Led pins on MM
+//#define RED_PIN 20    //Led pins on Mega
 //#define GREEN_PIN 21 
 //#define BLUE_PIN 22 
 
-//#define ECHO_TO_SERIAL  //NOTE that opening the serial window restarts the sketch & ruins your time setting!
+#define ECHO_TO_SERIAL  //NOTE that opening the serial window RESTARTS the sketch & ruins your time setting!
 
 RTC_DS1307 RTC;
 
@@ -41,9 +32,9 @@ void setup(void)
   pinMode(RTCPOWER_PIN, HIGH);   //some of my units use this pin for powering the RTC, need to set high for I2c coms.
   Wire.begin();
   RTC.begin();  
-  RTC.adjust(DateTime(__DATE__, __TIME__));
+  //RTC.adjust(DateTime(__DATE__, __TIME__));
   DateTime now = RTC.now();
-delay(5);
+
 #if defined(ECHO_TO_SERIAL)  
 Serial.begin(9600);
   Serial.print(now.year(), DEC);
